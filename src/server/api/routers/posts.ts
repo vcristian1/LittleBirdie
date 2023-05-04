a91fa1389471837bb4aa912a5f99312d1d73155b
Cristian Vargas <cristian.v0223@gmail.com>
2023-05-04 14:56:51 -0500

    resolving deployment error

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -29,7 +29,7 @@ export const postsRouter = createTRPCRouter({
     return posts.map((post) => {
       const author = users.find((user) => user.id === post.authorId);
 
-      if (!author || !author.name) throw new TRPCError({
+      if (!author || !author.name) throw new RTCError({
         code: "INTERNAL_SERVER_ERROR",
         message: "Author for post not found",
       });

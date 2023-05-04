1b044f95283af27aef854840914607b089edd574
Cristian Vargas <cristian.v0223@gmail.com>
2023-05-04 11:22:46 -0500

    added post route to Router, added post, and rendered on index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,10 @@ import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-  
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
+
   return (
     <>
       <Head>
@@ -19,6 +20,9 @@ const Home: NextPage = () => {
         <div>
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
         </div>
       </main>
     </>

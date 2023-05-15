2557711b201116d4142533cb03955d2a8f7f0dbb
Cristian Vargas <cristian.v0223@gmail.com>
2023-05-15 12:18:59 -0500

    added orderBy to posts.ts to order the posts in desc by createdAt

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -17,6 +17,11 @@ export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
+      orderBy: [
+        {
+          createdAt: "desc"
+        }
+      ]
     });
 
     const users = ( 
